76e287a18d4be8caca76897cbf86ac3af6254b95
KevinBuzit <buzitk@gmail.com>
2018-07-05 11:41:03 +0200

    Card component externalized

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { CribListingComponent } from './crib-listing/crib-listing.component';
+import { CribCardComponent } from './crib-card/crib-card.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CribListingComponent
+    CribListingComponent,
+    CribCardComponent
   ],
   imports: [
     BrowserModule
