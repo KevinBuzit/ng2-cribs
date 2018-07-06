17ffda1b7279e10b4e52d5603442a26c1e864a22
KevinBuzit <buzitk@gmail.com>
2018-07-06 14:42:20 +0200

    addCrib service

--- src/app/services/cribs.service.ts
+++ src/app/services/cribs.service.ts
@@ -1,16 +1,23 @@
 import { Injectable } from '@angular/core';
 import { Http } from '@angular/http';
 import { map } from "rxjs/operators";
+import { Subject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CribsService {
 
+  public newCribSubject = new Subject<any>();
   constructor(private http: Http) { }
 
   getAllCribs() {
     return this.http.get('data/cribs.json')
       .pipe(map(res => res.json()))
+  }
+
+  addCrib(data){
+    data.image = 'default-crib';
+    this.newCribSubject.next(data)
   }
 }
